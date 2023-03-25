88c61e790f0a1f10759ae27aab956b3e58c32f39
MarquesGitHub <jose.marques.mailbox@gmail.com>
2023-03-25 16:10:43 +0000

    calcular a área de um círculo

---
+++ Teste.java
@@ -0,0 +1,10 @@
+public class Teste {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        // calcular a área de um círculo
+        double raio = 10;
+        double area = Math.PI * raio * raio;
+        System.out.println("A área do círculo é: " + area);
+
+    }
+}
